85a5ebae4a01e5bd94b84a7e25e1e6d9b8222658
samir <karavadarasamir92@gmail.com>
2026-01-10 14:01:16 +0530

    Nothing

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
-from flask import Flask, request, send_file,redirect,url_for
+# from flask import Flask, request, send_file,redirect,url_for
 # Flask --> help for create a webpage
 # request --> read data for html
 # send_file -->send python file to browser
 # import Emaillll
 # import connect_with_storage
 
-app = Flask(__name__)  # create a flask application
+# app = Flask(__name__)  # create a flask application
 # __name__ tells Flask this file is the main app
 
 
 # ---------------- HOME PAGE ----------------
-@app.route('/')  #When user opens http://127.0.0.1:5000/, this runs
-def home():
-    return send_file('index.html')  # Sends index.html file to browser
+# @app.route('/')  #When user opens http://127.0.0.1:5000/, this runs
+# def home():
+    # return send_file('index.html')  # Sends index.html file to browser
 # def login():
 #     return send_file('login.html')
 
 
 # ---------------- FORM SUBMIT ----------------
-@app.route('/submit', methods=[ 'POST'])
+# @app.route('/submit', methods=[ 'POST'])
 # URL /submit
 # Accepts POST request only
 # Form action must be /submit
 
-def submit():
+# def submit():
     # read username from web page
     # and .get mathod is avoid if it is null
     # default store a string
@@ -35,13 +35,44 @@ def submit():
     # return  redirect(url_for('http://127.0.0.1:5000/'))
     # same page are closed and new page are loded 
     # url_for is   auto update page
-    redirect(url_for('home'))
+
+    # name = request.form['option']
+    # print(name)
+    # return redirect(url_for('home'))
 
     
 
 
 # ---------------- RUN server ----------------
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # start web server
     # if any error then shows it
+    # app.run(debug=True)
+
+
+
+
+
+
+
+
+
+from flask import Flask, request, send_file, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return send_file('index.html')
+
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    option = request.form['option']
+    print(option)
+    return redirect(url_for('home'))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
